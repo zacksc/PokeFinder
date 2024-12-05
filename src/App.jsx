30f35ffcff554c1629c6287911170cae8c27cc70
Ezequiel <53439417+zacksc@users.noreply.github.com>
2024-12-04 22:05:51 -0300

    primeiro commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [image, setImage] = useState("");
+  const [id, setId] = useState("");
+const [name, setName] = useState("");
+
+  const fetchCharacter = async () => {
+    try {
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${id}`
+      );
+      const data = await response.json();
+      setImage(data.sprites.front_default || "https://via.placeholder.com/150");
+     setName(data.name)
+    } catch {
+      alert("Algo deu errado! Tente outro ID.");
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+     <h1>{name}</h1>
+      <img src={image} alt="Personagem" />
+      <input
+        type="text"
+        value={id}
+        onChange={(e) => setId(e.target.value)}
+        placeholder="Digite o ID ou nome"
+      />
+      <button onClick={fetchCharacter}>Buscar</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
